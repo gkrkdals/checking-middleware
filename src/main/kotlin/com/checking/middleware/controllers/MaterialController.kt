1b309dddaa6c765075c34ebf651a9dd391266667
gkrkdals <hagangmin@gmail.com>
2025-04-28 06:26:08 +0900

    make it better

--- src/main/kotlin/com/checking/middleware/controllers/MaterialController.kt
+++ src/main/kotlin/com/checking/middleware/controllers/MaterialController.kt
@@ -6,6 +6,7 @@ import com.checking.middleware.services.MaterialService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/apiproxy")
 class MaterialController(val materialService: MaterialService) {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello world"
+    }
 
     //----- 입고정보 -----//
     @Operation(
